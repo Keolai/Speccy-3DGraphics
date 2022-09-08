86d4532b51113c05a6135d584d883eb5d50523ff
Keolailani Rose <krose102202@gmail.com>
2022-09-07 19:43:45 -0500

    fixed spacing issues

--- src/twodmatrixlib.asm
+++ src/twodmatrixlib.asm
@@ -25,76 +25,76 @@ Mult = adrStart + #0a
 
 ;; a1 = b, a2 = c, a3 = d, a4 = e h = x, l = y
 loadMatrix:
-push a 
-ld a, b
-ld a1, a
-ld a, c
-ld a2, a
-ld a, d
-ld a3, a
-ld a, e
-ld a4, a
-ld a,h
-ld cX, a
-ld pX, a
-ld a, l
-ld cY, a
-ld pY, a
-pop a
-ret
+    push a 
+    ld a, b
+    ld a1, a
+    ld a, c
+    ld a2, a
+    ld a, d
+    ld a3, a
+    ld a, e
+    ld a4, a
+    ld a,h
+    ld cX, a
+    ld pX, a
+    ld a, l
+    ld cY, a
+    ld pY, a
+    pop a
+    ret
 
 solveMatrix:
-call getMultiplier
-call getSolutions
-ret
+    call getMultiplier
+    call getSolutions
+    ret
 
 ;;bc = hl/e
 ;;hl = b * c
 getMultiplier:
-push a
-ld a, a1
-ld e,a
-ld hl, a3
-call div
-ld a,c
-ld Mult,a
-ld b, a
-ld a, a2
-ld c, a
-call slow_mult
-ld a, a4
-sub a, l        ;;need to handle possible negatives
-ld a4, a
-ld a, cX
-ld b, a
-ld a, Mult
-ld c, a
-call slow_mult
-ld a, cY
-sub a, l
-ld cY, a
-pop a
-ret
+    push a
+    ld a, a1
+    ld e,a
+    ld hl, a3
+    call div
+    ld a,c
+    ld Mult,a
+    ld b, a
+    ld a, a2
+    ld c, a
+    call slow_mult
+    ld a, a4
+    sub a, l        ;;need to handle possible negatives
+    ld a4, a
+    ld a, cX
+    ld b, a
+    ld a, Mult
+    ld c, a
+    call slow_mult
+    ld a, cY
+    sub a, l
+    ld cY, a
+    pop a
+    ret
 
 
 getSolutions:
-push a
-ld a,cY
-ld hl,a
-ld a, a4
-ld e,a
-call div
-ld a, c
-ld sX2
-ld b, a2
-call slow_mult
-ld a, cX
-sub a,hl
-ld hl,a
-ld a,a1
-ld e,a
-call div
-ld a,c
-ld sX1,a
-pop a
-ret
+    push a
+    ld a,cY
+    ld hl,a
+    ld a, a4
+    ld e,a
+    call div
+    ld a, c
+    ld sX2
+    ld b, a2
+    call slow_mult
+    ld a, cX
+    sub a,hl
+    ld hl,a
+    ld a,a1
+    ld e,a
+    call div
+    ld a,c
+    ld sX1,a
+    pop a
+    ret
