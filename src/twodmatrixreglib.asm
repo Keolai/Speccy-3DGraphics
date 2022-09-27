be5ae78d28511612742b622d766a96f769313d09
Keolailani Rose <krose102202@gmail.com>
2022-09-26 21:16:08 -0500

    added all routines to solve for x2

--- src/twodmatrixreglib.asm
+++ src/twodmatrixreglib.asm
@@ -7,6 +7,9 @@
 ;; a1 = b, a2 = c, a3 = d, a4 = e h = x, l = y
 ;;change ALL ld label, a to ld hl, label, ld (hl), a
 multiplier EQU $d000
+numerator EQU $d001
+denominator EQU $d002
+xTwo EQU $d003
 
 loadMatrix:
     push af
@@ -24,6 +27,7 @@ loadMatrix:
 solveMatrix:
     call getMultiplier
     call getSecondSolNum
+    call getSecondSolDen
     ;call getSolutions
     ret
 
@@ -38,7 +42,7 @@ getMultiplier:
     ld a,c             ;load mutliple into a (should be stored in c)
     ld (hl),a           ;store at address
     ret 
-getSecondSolNum:
+getSecondSolNum:        ;x2 numerator
     ld hl,multiplier
     ld a,(hl)
     exx
@@ -58,9 +62,41 @@ getSecondSolNum:
     ld hl,numerator
     ld (hl),a       ;store in numerator    ;NEED to HANDLE NEGATIVES for now just reversed
     ret
+getSecondSolDen:            ;x2 denominator
+    ld hl, multiplier       ;lets get M*a2 first
+    ld a,(hl)
+    exx
+    push bc
+    exx
+    pop bc                  ;a2 now in b
+    ld c,a
+    call slow_mult          ;hl now stores m*a2
+    exx
+    push de
+    exx 
+    pop de                  ;e now has a4
+    ld a,e
+    ld b,l
+    sub b
+    call c, negativeHandler  ;gets absolute value of negative if negative was created
+    ld hl,denominator
+    ld (hl),a       ;store in numerator    ;NEED to HANDLE NEGATIVES for now just reversed
+    ret
+    ;;bc = hl/e
+getSecondSol:
+    ld hl,denominator
+    ld a,(hl)
+    ld e,a
+    ld hl,numerator
+    ld a,(hl)
+    ld l,a
+    call div
+    ld hl,xTwo
+    ld a,b
+    ld (hl),a
+    ret
+    
 negativeHandler:
     xor $ff
+    inc a           ;weird, may need to change
     ret
-
-numerator: db $00
-denominator: db $00
